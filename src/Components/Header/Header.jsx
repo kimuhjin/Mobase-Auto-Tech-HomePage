7e8dce5046d00921b2dfc9497c437dfe5cd71a8b
KimUhJin <ajin950714@naver.com>
2021-04-10 16:19:38 +0900

    2차 샘플 디자인 수정 및 배너 animation 추가

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,28 @@
 import './Header.css';
 import logo from "../../asset/image/logo.png"
-import React from "react"
+import React,{useState} from "react"
 
 
 
 function Header() {
-   
+   const [clicked,setClicked] = useState("")
+
+  //  setInterval(() => {
+  //     let presentPosition = document.querySelector('html').scrollTop+80;
+  //     if(presentPosition>=935){
+  //       setClicked("company")
+  //     }
+  //     if(presentPosition >=document.getElementById("product-section").offsetTop
+  //     &&presentPosition <document.getElementById("recruit-section").offsetTop
+  //     ){
+  //       setClicked("product")
+  //     }
+  //      if(presentPosition >=document.getElementById("recruit-section").offsetTop){
+  //       setClicked("recruit")
+  //     }
+  //  }, 1000);
      function handleScrollTo(e) {
+       setClicked(e.target.id)
        const locationName = `${e.target.id}-section`;
             let menuHeight = 80;
             let location =
@@ -25,9 +41,9 @@ function Header() {
     <div className="header-container">
       <img className="logo" src={logo} alt="MOBASE AUTO TECH Logo"/>
       <div className="menu-container">
-      <div className="menu" id="company" onClick={handleScrollTo}>회사 소개</div>
-      <div className="menu" id="product" onClick={handleScrollTo}>제품</div>
-      <div className="menu" id="recruit" onClick={handleScrollTo}>인재경영</div>
+      <div className={`menu ${clicked==="company" ?"clicked":""}`} id="company" onClick={handleScrollTo}>회사 소개</div>
+      <div className={`menu ${clicked==="product" ?"clicked":""}`} id="product" onClick={handleScrollTo}>제품</div>
+      <div className={`menu ${clicked==="recruit" ?"clicked":""}`} id="recruit" onClick={handleScrollTo}>인재경영</div>
       </div>
     </div>
   );
--- src/Pages/MainPage/MainPage.jsx
+++ src/Pages/MainPage/MainPage.jsx
@@ -6,8 +6,19 @@ import Recruit from "./Sections/Recruit/Recruit.jsx"
 function MainPage() {
   return (
     <div className="main-page-container">
+    <div className="main-page-banner-container">
     <div className="main-page-banner1"/>
     <div className="main-page-banner2"/>
+    {/* <div className="main-page-banner-customer">
+     <img className="logo" src="http://www.seoyonelec.com/data/file/client/thumb-3422075460_pgSCezbP_e9e177ff4a21d98dc1d7b12a799c27c853651447_168x60.jpg" alt="MOBASE AUTO TECH Logo"/>
+      <img className="logo" src="http://www.seoyonelec.com/data/file/client/thumb-3422075460_Sfn9WoBm_b08e0e0fc3ea1bbae59f56c6646f441cfaf47e3f_168x60.jpg" alt="MOBASE AUTO TECH Logo"/>
+       <img className="logo" src="http://www.seoyonelec.com/data/file/client/thumb-3422075460_3QCmExVj_0dae0f81d284cb642b6b1f5e2a055b08d4106e9c_168x60.jpg" alt="MOBASE AUTO TECH Logo"/>
+        <img className="logo" src="http://www.seoyonelec.com/data/file/client/thumb-3422075460_5pyISg8r_db1b004c7e3a45f0e505655b78bfd6042726e625_168x60.jpg" alt="MOBASE AUTO TECH Logo"/>
+         <img className="logo" src="http://www.seoyonelec.com/data/file/client/thumb-3422075460_rK54ipQN_ef5849a2edb9afc87de0da821e66dc5c3cb87170_168x60.jpg" alt="MOBASE AUTO TECH Logo"/>
+          <img className="logo" src="http://www.seoyonelec.com/data/file/client/thumb-3422075460_SML1oUAj_384a91126f31748363efca6bf945cab7b5d5b6d9_168x60.jpg" alt="MOBASE AUTO TECH Logo"/>
+    </div> */}
+    <div/>
+    </div>
     
 <div className="main-page-section" id="company-section">
 <Company/>
