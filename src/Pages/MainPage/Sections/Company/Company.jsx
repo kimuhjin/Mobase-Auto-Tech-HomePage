549a7ee674b5d031b5e01e5c142ec981b084eca7
KimUhJin <ajin950714@naver.com>
2021-04-10 17:27:39 +0900

    카카오맵 추가 및 이메일 지원 버튼 추가

--- src/Pages/MainPage/Sections/Company/Company.jsx
+++ src/Pages/MainPage/Sections/Company/Company.jsx
@@ -1,7 +1,12 @@
 import './Company.css';
+import KakaoMap from "../../../../Components/KakaoMap/KakaoMap"
 import React,{useState} from "react"
 function Company() {
   const [content,setContent]=useState(1)
+
+  
+       
+
   return (
     <div className="company-container">
     <div className="company-sub-header">
@@ -11,11 +16,20 @@ function Company() {
      <div className={`company-sub-header-menu ${content===2 ?"clicked":""}`} onClick={()=>setContent(2)}>
    사업영역
     </div>
+<div className={`company-sub-header-menu ${content===3 ?"clicked":""}`} onClick={()=>setContent(3)}>
+   위치
+    </div>
+
     </div>
     <div className="company-contents">
      {content ===1 &&<div className="company-image-1"></div>}
         {content ===2 &&<div className="company-image-2"></div>}
-     {content ===3 &&("Company content 3")}
+      
+     {content ===3 &&<KakaoMap  location={{ x: 37.21475486667424, y:127.09381401782406 }}
+                    infoWindowMessage={'모베이스 오토테크'}
+                    markerOn={true}
+                    infoWindowOn={true}
+                    level={4}/>}
      </div>
     </div>
   );
--- src/Pages/MainPage/Sections/Recruit/Recruit.jsx
+++ src/Pages/MainPage/Sections/Recruit/Recruit.jsx
@@ -11,10 +11,26 @@ function Recruit() {
      <div className={`recruit-sub-header-menu ${content===2 ?"clicked":""}`} onClick={()=>setContent(2)}>
  인재육성
     </div>
+         <div className={`recruit-sub-header-menu ${content===3 ?"clicked":""}`} onClick={()=>setContent(3)}>
+ 지원하기
+    </div>
     </div>
     <div className="recruit-contents">
-      {content ===1 &&<div className="recruit-image-1"></div>}
-      {content ===2 &&<div className="recruit-image-2"></div>}
+      {content ===1 &&<div className="recruit-image-1"/>}
+      {content ===2 &&<div className="recruit-image-2"/>}
+      {content ===3 &&(
+      <>
+      <div className="recruit-infomation-container">
+      <div className="recruit-image-recruit1"/>
+      <div className="recruit-infomation">
+      <a href="mailto:recruit@mobase-autotech.com?subject=상반기 모베이스 오토테크 신입사원 지원&body=상반기 모베이스 오토테크 신입사원 지원 서류입니다." className="recruit-email-btn">
+      이메일 지원하기
+      </a>
+      </div>
+      <div className="recruit-image-recruit2"/>
+      </div>
+      </>
+      )}
      </div>
     </div>
   );
