dcb8df331862975b42d64f477313687849a151ed
KimUhJin <ajin950714@naver.com>
2021-04-11 17:56:24 +0900

    윤리경영, 연구개발 컨텐츠 추가

--- src/Pages/MainPage/Sections/Business/Business.jsx
+++ src/Pages/MainPage/Sections/Business/Business.jsx
@@ -22,7 +22,42 @@ function Business() {
           품질/환경안전보건
         </div>
       </div>
-      <div className="business-contents">{content === 1 && <div></div>}</div>
+      <div className="business-contents">
+        {content === 1 && (
+          <div className="business-ethics-container">
+            <div className="business-ethics-title-line" />
+            <div className="business-ethics-title">
+              대표이사 윤리경영 메세지
+            </div>
+            <div className="business-ethics-sub-title">
+              안녕하십니까?
+              <br />
+              모베이스 오토테크 대표이사 박노헌입니다.
+            </div>
+            <div className="business-ethics-article">
+              당사 경영진은 윤리경영의 중요성을 실감하고 모든 구성원이 공감하며
+              실천할 수 있는 기업문화를 정착시키기 위해 다음과 같은 노력을
+              기울이고 있습니다.
+              <div className="business-ethics-article-box">
+                <div className="flex">
+                  <div className="color-blue">· 첫째</div>&nbsp;&nbsp;공정한
+                  경쟁과 균등한 처우를 보장하기 위해 윤리규칙, 행동지침,
+                  실천지침을 통해 업무를 수행하고 있습니다.
+                </div>
+                <div className="flex">
+                  <div className="color-blue">· 둘째</div>&nbsp;&nbsp;비윤리적인
+                  행위에 대한 감찰 및 실천제도를 운영하여 직/간접 참여자에게
+                  책임과 처벌을 강력하게 추진하고 있습니다.
+                </div>
+              </div>
+              글로벌 경쟁시대에서 윤리경영은 기업의 존속과 성장을 위한
+              필수사항입니다. 앞으로도 저희 모베이스 오토테크는 사내 구성원들은
+              물론 대외적으로 신뢰와 존경을 받을 수 있는 기업으로 거듭나기
+              위하여 최선을 다할 것입니다.
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- src/Pages/MainPage/Sections/Rnd/Rnd.jsx
+++ src/Pages/MainPage/Sections/Rnd/Rnd.jsx
@@ -9,7 +9,7 @@ function Rnd() {
           className={`rnd-sub-header-menu ${content === 1 ? "clicked" : ""}`}
           onClick={() => setContent(1)}
         >
-          R&D센터
+          R&D 센터
         </div>
         <div
           className={`rnd-sub-header-menu ${content === 2 ? "clicked" : ""}`}
@@ -24,7 +24,40 @@ function Rnd() {
           연구성과
         </div>
       </div>
-      <div className="rnd-contents">{content === 1 && <div></div>}</div>
+      <div className="rnd-contents">
+        {content === 1 && (
+          <div className="rnd-center-container">
+            <div className="rnd-center-title">
+              모베이스 오토테크는 국내 1위, Global Top 10을 이루기 위해 <br />
+              다음과 같은 전약으로 연구를 진행하고 있습니다.
+            </div>
+            <div className="rnd-center-sub-title">
+              R&D 목표 달성을 위한 전략
+            </div>
+            <div className="rnd-center-sub-title-line" />
+            <div className="rnd-center-article-title">
+              저희 모베이스 오토테크는 활발한 R&D투바와 최고의 교육과정으로
+              연구인재를 육성하고 있습니다.
+              <br />
+              <br />
+            </div>
+            <div className="rnd-center-article">
+              최신의 자동차 기술을 반영하고, 시장을 선도하기 위한 끊임없는
+              연구로 신기술과 신제품을 개발하며, 더불어 현재 생산중인 제품의
+              소형화, 저비용화, 모듈화를 위한 선행 및 양산 개발을 통하여 고객의
+              만족도를 극대화 하고 있습니다.
+              <br />
+              <br />
+              오늘날의 자동차는 시스템의 전자화 추세에 따라 소프트웨어의 활용이
+              중요해졌습니다. 저희 모베이스 오토테크는 이것의 중요성을
+              최우선으로 인식하여 소프트웨어의 표준화, 글로벌 플랫폼의 적용,
+              Test Case의 개발과 더불어 운전자의 안전을 고려한
+              ISO26262(기능안전프로세스), 전세계 지역별 대응을 위환
+              A-SPICE프로세스 인증도 준비하고 있습니다.
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
