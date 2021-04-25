5a38332012906df98b1c2e8e7e65652a62050fe4
KimUhJin <ajin950714@naver.com>
2021-04-25 20:20:39 +0900

    package.json 롤백

---
+++ backUpforPackage.js
@@ -0,0 +1,7 @@
+// "build": "react-scripts build",
+// "test": "react-scripts test",
+// "eject": "react-scripts eject",
+// "cname": "echo mobaseautotech.com > build\\CNAME",
+// "nojekyll": "echo > build\\.nojekyll",
+// "predeploy": "npm run build && npm run cname && npm run nojekyll",
+// "deploy": "gh-pages --add --dotfiles -d build"
