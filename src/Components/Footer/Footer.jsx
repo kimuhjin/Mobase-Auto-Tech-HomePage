dcf8f6b1d865543a7a55b28c89a5216d2035ec74
KimUhJin <ajin950714@naver.com>
2021-05-27 22:23:29 +0900

    1차 오탈자 수정

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,10 +5,10 @@ function Footer() {
     <div className="footer-container">
       <div className="footer-logo-box">
         <img className="footer-logo" src={logo} alt="MOBASE AUTO TECH Logo" />
-        주식회사 모베이스 오토테크
+        모베이스오토테크 주식회사
       </div>
       <div className="footer-description">
-        상호 : (주)모베이스오토테크 &nbsp;&nbsp;대표 : 박노헌
+        상호 : 모베이스오토테크(주) &nbsp;&nbsp;대표 : 박노헌
         &nbsp;&nbsp;사업자등록번호 : 131-81-95154
         <br />
         경기 화성시 동탄첨단산업1로73 &nbsp;&nbsp;TEL:032-500-5874
